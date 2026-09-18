f6777f0c86d4c9cbafb0ed4f36c0fa7b4042c83c
Michal Haftarczyk <287037@student.pwr.edu.pl>
2026-09-18 15:28:46 +0200

    feat: implemented mock function for the sensor, since i dont have it at home now

---
+++ Inc/sensor.h
@@ -0,0 +1,13 @@
+/*
+ * sensor.h
+ *
+ *  Created on: 18 wrz 2026
+ *      Author: Michał
+ */
+
+#ifndef SENSOR_H_
+#define SENSOR_H_
+
+
+
+#endif /* SENSOR_H_ */
---
+++ Src/sensor.c
@@ -0,0 +1,8 @@
+/*
+ * sensor.c
+ *
+ *  Created on: 18 wrz 2026
+ *      Author: Michał
+ */
+
+
